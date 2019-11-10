ed3d7ddafaddecff3b8011c5891d90e4fe228256
Klaudia <brysiewiczk@gmail.com>
2019-11-10 19:24:46 +0100

    currency - exchange rate input

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import CurrencyConverter from './currencyConverter';
 
 const App = () => {
     return (
-        <Grid container className="App">
-            <Grid>
+        <Grid container style={styles.container} className="App">
+            <Grid item xs={12}>
+                <Typography variant="h4">Przelicznik walutowy</Typography>
+            </Grid>
+            <Grid item xs={12} md={4}>
                 <CurrencyConverter />
             </Grid>
         </Grid>
     );
 };
 
+const styles = {
+    container: {
+        margin: '40px',
+    },
+}
 export default App;
--- src/components/currencyConverter.js
+++ src/components/currencyConverter.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
+import Input from '@material-ui/core/Input';
 
 const CurrencyConverter = () => {
+    const [value, setVaule] = useState(1);
+    const [euroValue, setEuroValue] = useState('');
+    const [exchangeRate, setExchangeRate] = useState('');
+    const calculateValue = (event) => {
+        const value = event.target.value;
+        setVaule(value);
+        setEuroValue(value * exchangeRate);
+    };
+    const calculateExchangeRate = (event) => {
+        const exchangeRate = event.target.value;
+        setExchangeRate(exchangeRate)
+        setEuroValue(exchangeRate * value);
+    };
     return (
         <Card>
-            <CardContent>CurrencyConverter</CardContent>
+            <CardContent>
+                <Typography>Przelicznik:</Typography>
+                <Input value={exchangeRate} onChange={(event) => calculateExchangeRate(event)} type="number" />
+                { exchangeRate
+                ? (
+                    <Fragment>
+                        <Typography>Kwota:</Typography>
+                        <Input value={value} onChange={(event) => calculateValue(event)} type="number" />
+                    </Fragment>
+                ) : null}
+                {(value && exchangeRate)
+                ? (<Typography>{value} PLN = {euroValue} €</Typography>)
+                : null}
+            </CardContent>
         </Card>
     );
 };
